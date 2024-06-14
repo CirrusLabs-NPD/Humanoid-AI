10d98b0657c0b16b1f856e662323812504cc02f3
Owais51 <123532004+Owais51@users.noreply.github.com>
2024-06-14 10:59:20 -0400

    MongoDB connection

--- apps/humanoid-ai-backend/src/app/app.module.ts
+++ apps/humanoid-ai-backend/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
